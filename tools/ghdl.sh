6c37ef7732badd26acf0c82c3b75f9734678f2cf
Taner Öksüz <taner.oeksuez@outlook.com>
2020-08-22 16:06:08 +0200

    installation script for ghdl fixed.

--- tools/ghdl.sh
+++ tools/ghdl.sh
@@ -3,7 +3,7 @@
 GHDL_PATH=/opt/ghdl
 
 sudo apt-get -y install git wget build-essential automake autoconf autoconf-archive \
-  flex check llvm-5.0-dev clang-5.0 llvm-dev clang gnat pkg-config zlib1g-dev curl \
+  flex check clang llvm-dev libclang-dev gnat pkg-config zlib1g-dev curl \
   texinfo ruby libmpfr-dev libmpc-dev libgmp-dev
 
 if [ -d "ghdl" ]; then
@@ -17,7 +17,7 @@ cd ghdl
 mkdir build
 cd build
 
-../configure --prefix=$GHDL_PATH --with-llvm-config=/usr/bin/llvm-config-5.0
+../configure --prefix=$GHDL_PATH --with-llvm-config=/usr/bin/llvm-config
 
 make -j$(nproc)
 sudo make install
