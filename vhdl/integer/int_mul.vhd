89b183fbbd3d69c3b190caad57dffcf8a92b4ef1
Taner Öksüz <taner.oeksuez@outlook.com>
2020-12-14 20:14:31 +0100

    int multiply unit optimized.

--- vhdl/integer/int_mul.vhd
+++ vhdl/integer/int_mul.vhd
@@ -26,12 +26,6 @@ end int_mul;
 
 architecture behavior of int_mul is
 
-	signal r_1 : int_mul_reg_type_1 := init_int_mul_reg_1;
-	signal r_2 : int_mul_reg_type_2 := init_int_mul_reg_2;
-
-	signal rin_1 : int_mul_reg_type_1 := init_int_mul_reg_1;
-	signal rin_2 : int_mul_reg_type_2 := init_int_mul_reg_2;
-
 	signal r : int_mul_reg_type := init_int_mul_reg;
 
 	signal rin : int_mul_reg_type := init_int_mul_reg;
@@ -41,144 +35,66 @@ begin
 	FAST : if mul_performance = true generate
 
 		process(int_mul_i)
-			variable op    : mul_operation_type;
-			variable word  : std_logic;
-			variable neg   : std_logic;
-			variable aa    : std_logic_vector(63 downto 0);
-			variable bb    : std_logic_vector(63 downto 0);
-			variable ready : std_logic;
+			variable data1      : std_logic_vector(64 downto 0);
+			variable data2      : std_logic_vector(64 downto 0);
+			variable result     : std_logic_vector(129 downto 0);
+			variable op         : mul_operation_type;
+			variable op1_signed : std_logic;
+			variable op2_signed : std_logic;
+			variable word       : std_logic;
+			variable ready      : std_logic;
 
 		begin
+			data1 := '0' & int_mul_i.data1;
+			data2 := '0' & int_mul_i.data2;
 			op    := int_mul_i.op;
 			word  := int_mul_i.word;
-			neg   := '0';
+
+			op1_signed := op.alu_mul or
+										op.alu_mulh or
+										op.alu_mulhsu;
+			op2_signed := op.alu_mul or
+										op.alu_mulh;
 			ready := int_mul_i.enable and
-								(int_mul_i.op.alu_mul or int_mul_i.op.alu_mulh or
-								int_mul_i.op.alu_mulhu or int_mul_i.op.alu_mulhsu);
+								(op.alu_mul or
+								 op.alu_mulh or
+								 op.alu_mulhu or
+								 op.alu_mulhsu);
 
-			if op.alu_mulhu = '1' then
-				aa := int_mul_i.data1;
-			else
-				if int_mul_i.data1(63) = '1' then
-					aa  := std_logic_vector(-signed(int_mul_i.data1));
-					neg := not neg;
+			if op1_signed = '1' then
+				if word = '1' then
+					data1(64 downto 32) := (others => data1(31));
 				else
-					aa := int_mul_i.data1;
+					data1(64) := data1(63);
+				end if;
+			end if;
+			if op2_signed = '1' then
+				if word = '1' then
+					data2(64 downto 32) := (others => data2(31));
+				else
+					data2(64) := data2(63);
 				end if;
 			end if;
 
-			if (op.alu_mulhu or op.alu_mulhsu) = '1' then
-				bb := int_mul_i.data2;
-			else
-				if int_mul_i.data2(63) = '1' then
-					bb  := std_logic_vector(-signed(int_mul_i.data2));
-					neg := not neg;
-				else
-					bb := int_mul_i.data2;
-				end if;
-			end if;
-
-			rin_1.op <= op;
-			rin_1.word <= word;
-			rin_1.neg <= neg;
-			rin_1.aa <= aa;
-			rin_1.bb <= bb;
-			if int_mul_i.clear = '1' then
-				rin_1.ready <= '0';
-			else
-				rin_1.ready <= ready;
-			end if;
-
-			lzc_i.a <= X"0000000000000000";
-
-		end process;
-
-		process(r_1, int_mul_i)
-			variable op    : mul_operation_type;
-			variable word  : std_logic;
-			variable neg   : std_logic;
-			variable aa    : std_logic_vector(63 downto 0);
-			variable bb    : std_logic_vector(63 downto 0);
-			variable rr    : std_logic_vector(127 downto 0);
-			variable ready : std_logic;
-
-		begin
-			op    := r_1.op;
-			word  := r_1.word;
-			neg   := r_1.neg;
-			aa    := r_1.aa;
-			bb    := r_1.bb;
-			ready := r_1.ready;
-
-			rr := std_logic_vector(unsigned(aa) * unsigned(bb));
-
-			rin_2.op <= op;
-			rin_2.word <= word;
-			rin_2.neg <= neg;
-			rin_2.rr <= rr;
-			if int_mul_i.clear = '1' then
-				rin_2.ready <= '0';
-			else
-				rin_2.ready <= ready;
-			end if;
-
-		end process;
-
-		process(r_2, int_mul_i)
-			variable op     : mul_operation_type;
-			variable word   : std_logic;
-			variable neg    : std_logic;
-			variable rr     : std_logic_vector(127 downto 0);
-			variable result : std_logic_vector(63 downto 0);
-			variable ready  : std_logic;
-
-		begin
-			op    := r_2.op;
-			word  := r_2.word;
-			neg   := r_2.neg;
-			rr    := r_2.rr;
-			ready := r_2.ready;
-
-			if neg = '1' then
-				rr := std_logic_vector(-signed(rr));
-			end if;
-
-			if op.alu_mul = '1' then
-				result := rr(63 downto 0);
-			else
-				result := rr(127 downto 64);
-			end if;
+			result := std_logic_vector(signed(data1) * signed(data2));
 
 			if word = '1' then
 				result(63 downto 32) := (others => result(31));
 			end if;
 
-			int_mul_o.result <= result;
+			if op.alu_mul = '1' then
+				int_mul_o.result <= result(63 downto 0);
+			else
+				int_mul_o.result <= result(127 downto 64);
+			end if;
+
 			if int_mul_i.clear = '1' then
 				int_mul_o.ready <= '0';
 			else
 				int_mul_o.ready <= ready;
 			end if;
 
-		end process;
-
-		process(clock)
-		begin
-			if rising_edge(clock) then
-
-				if reset = '0' then
-
-					r_1 <= init_int_mul_reg_1;
-					r_2 <= init_int_mul_reg_2;
-
-				else
-
-					r_1 <= rin_1;
-					r_2 <= rin_2;
-
-				end if;
-
-			end if;
+			lzc_i.a <= X"0000000000000000";
 
 		end process;
 
